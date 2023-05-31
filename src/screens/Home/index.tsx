63242236754cd7656972e718914747cb749314cb
Fernanda Paiva <fernandapaiva11@hotmail.com>
2023-05-31 10:49:26 -0300

    : Adicionando Tela Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,59 @@
 import React from 'react';
-import {Container} from './styles';
+import {Container, RowView, FirstRow, TitleBlack, ValueCard, CardGreen, CardPurple, TitleCard, NumberCard, Encapsulate, MainCards, Separator5, Separator20, Separator40} from './styles';
 import WalletIcon from '../../assets/Wallet';
+import Transfer from '../../assets/Transfer';
+import Bills from '../../assets/Bills';
+import Withdraw from '../../assets/Withdraw';
+import Reward from '../../assets/Reward';
+import Nearby from '../../assets/Nearby';
+import Notification from '../../assets/Notification';
 
 export default function Home () {
 
   return (
     <Container>
+      <FirstRow>
+        <TitleBlack>Dashboard</TitleBlack>
+        <Notification /> 
+      </FirstRow>
+      <MainCards>
+      <CardGreen>
+        <Separator5 />
+          <>
+          <TitleCard>Current Balance</TitleCard>
+          <Separator5 />
+          <ValueCard>$12,490,209</ValueCard>
+          </>
+          <Separator40 /> 
+          <Separator40 />
+          <Encapsulate>          
+            <NumberCard> °°°° °°°° °°°° 3901</NumberCard>
+          </Encapsulate>
+        </CardGreen>
+        <CardPurple>
+        <Separator5 />
+          <>
+          <TitleCard>Current Balance</TitleCard>
+          <Separator5 />
+          <ValueCard>$12,490,209</ValueCard>
+          </>
+          <Separator40 /> 
+          <Separator40 />
+          <Encapsulate>          
+            <NumberCard> °°°° °°°° °°°° 3901</NumberCard>
+          </Encapsulate>
+        </CardPurple>
+        </MainCards>
+      <RowView>
      <WalletIcon />
+     <Transfer />
+     <Bills />
+     </RowView>
+     <RowView>
+      <Withdraw />
+      <Reward />
+      <Nearby />
+     </RowView>
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,6 +1,80 @@
 import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+
+const {width} = Dimensions.get('window');
 
 export const Container = styled.View`
   flex: 1;
   background-color: #E5E5E5;
+`;
+
+export const Separator5 = styled.View`
+  margin-top: 5px;
+`;
+
+export const Separator20 = styled.View`
+  margin-top: 20px;
+`;
+
+export const Separator40 = styled.View`
+  margin-top: 40px;
+`;
+
+export const FirstRow = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+export const TitleBlack = styled.Text`
+  font-size: 18px;
+  color: #111111;
+  font-weight: bold;
+`;
+
+export const MainCards = styled.View`
+  flex-direction: row;
+`;
+
+export const CardGreen = styled.View`
+  width: 320px;
+  height: 210px;
+  border-radius: 30px;
+  background-color: #1ba37a;
+  padding: 20px;
+  margin-left: 20px;
+`;
+
+export const CardPurple = styled.View`
+  width: 320px;
+  height: 210px;
+  border-radius: 30px;
+  background-color: #5F59E1;
+  padding: 20px;
+  margin-left: 20px;
+`;
+
+export const Encapsulate = styled.View``;
+
+export const TitleCard = styled.Text`
+  font-size: 13px;
+  color: #ffffff;
+`;
+
+export const NumberCard = styled.Text`
+  font-size: 16px;
+  color: #ffffff;
+`;
+
+export const ValueCard = styled.Text`
+  font-size: 28px;
+  color: #ffffff;
+  font-weight: bold;
+`;
+
+export const RowView = styled.View`
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-evenly;
+  padding: 20px;
 `;
