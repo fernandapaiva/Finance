fe8cbac5c084464f870150c5e0732b64aed855a0
Fernanda Paiva <fernandapaiva11@hotmail.com>
2023-06-13 19:01:24 -0300

    : Estilizando FlatList

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,59 +1,82 @@
-import React from 'react';
-import {Container, RowView, FirstRow, TitleBlack, ValueCard, CardGreen, CardPurple, TitleCard, NumberCard, Encapsulate, MainCards, Separator5, Separator20, Separator40} from './styles';
-import WalletIcon from '../../assets/Wallet';
-import Transfer from '../../assets/Transfer';
-import Bills from '../../assets/Bills';
-import Withdraw from '../../assets/Withdraw';
-import Reward from '../../assets/Reward';
-import Nearby from '../../assets/Nearby';
-import Notification from '../../assets/Notification';
+import React from "react";
+import { FlatList, View } from "react-native";
+import {
+  Container,
+  RowView,
+  FirstRow,
+  TitleBlack,
+  ValueCard,
+  CardContainer,
+  TitleCard,
+  NumberCard,
+  Separator5,
+  Separator40,
+} from "./styles";
+import WalletIcon from "../../assets/Wallet";
+import Transfer from "../../assets/Transfer";
+import Bills from "../../assets/Bills";
+import Withdraw from "../../assets/Withdraw";
+import Reward from "../../assets/Reward";
+import Nearby from "../../assets/Nearby";
+import Notification from "../../assets/Notification";
 
-export default function Home () {
+export default function Home() {
+  const DATA = [
+    {
+      conta: "Current Balance",
+      saldo: "$12,490,209",
+      numero: "°°°° °°°° °°°° 3901",
+      colorCard: "green",
+    },
+    {
+      conta: "Current Balance",
+      saldo: "$12,490,209",
+      numero: "°°°° °°°° °°°° 3901",
+      colorCard: "purple",
+    },
+  ];
 
   return (
     <Container>
       <FirstRow>
         <TitleBlack>Dashboard</TitleBlack>
-        <Notification /> 
+        <Notification />
       </FirstRow>
-      <MainCards>
-      <CardGreen>
-        <Separator5 />
-          <>
-          <TitleCard>Current Balance</TitleCard>
-          <Separator5 />
-          <ValueCard>$12,490,209</ValueCard>
-          </>
-          <Separator40 /> 
-          <Separator40 />
-          <Encapsulate>          
-            <NumberCard> °°°° °°°° °°°° 3901</NumberCard>
-          </Encapsulate>
-        </CardGreen>
-        <CardPurple>
-        <Separator5 />
-          <>
-          <TitleCard>Current Balance</TitleCard>
-          <Separator5 />
-          <ValueCard>$12,490,209</ValueCard>
-          </>
-          <Separator40 /> 
-          <Separator40 />
-          <Encapsulate>          
-            <NumberCard> °°°° °°°° °°°° 3901</NumberCard>
-          </Encapsulate>
-        </CardPurple>
-        </MainCards>
-      <RowView>
-     <WalletIcon />
-     <Transfer />
-     <Bills />
-     </RowView>
-     <RowView>
-      <Withdraw />
-      <Reward />
-      <Nearby />
-     </RowView>
+      <Separator40 />
+      <View style={{minHeight: 210}}>
+        <FlatList
+          showsHorizontalScrollIndicator={false}
+          data={DATA}
+          horizontal
+          renderItem={({ item }) => (
+            <View style={{paddingHorizontal: 15}}>
+              <CardContainer ColorCard={item.colorCard}>
+                <Separator5 />
+                <>
+                  <TitleCard>{item.conta}</TitleCard>
+                  <Separator5 />
+                  <ValueCard>{item.saldo}</ValueCard>
+                </>
+                <Separator40 />
+                <NumberCard>{item.numero}</NumberCard>
+              </CardContainer>
+            </View>
+          )}
+        />
+        <Separator40 />
+      </View>
+      <View style={{flex: 1}}>
+        <RowView>
+          <WalletIcon />
+          <Transfer />
+          <Bills />
+        </RowView>
+        <RowView>
+          <Withdraw />
+          <Reward />
+          <Nearby />
+        </RowView>
+      </View>
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,21 +3,32 @@ import { Dimensions } from 'react-native';
 
 const {width} = Dimensions.get('window');
 
-export const Container = styled.View`
+const CardColors = (item: any) => {
+  switch(item){
+    case 'green': {
+      return '#1ba37a'
+    }
+    case 'purple': {
+      return '#5F59E1'
+    }
+  }
+}
+
+export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: #E5E5E5;
 `;
 
 export const Separator5 = styled.View`
-  margin-top: 5px;
+  height: 5px;
 `;
 
 export const Separator20 = styled.View`
-  margin-top: 20px;
+  height: 20px;
 `;
 
 export const Separator40 = styled.View`
-  margin-top: 40px;
+  height: 40px;
 `;
 
 export const FirstRow = styled.View`
@@ -32,29 +43,12 @@ export const TitleBlack = styled.Text`
   font-weight: bold;
 `;
 
-export const MainCards = styled.View`
-  flex-direction: row;
-`;
-
-export const CardGreen = styled.View`
-  width: 320px;
-  height: 210px;
+export const CardContainer = styled.View`
+  width: 290px;
   border-radius: 30px;
-  background-color: #1ba37a;
+  background-color: ${(props: any) => CardColors(props.ColorCard)};
   padding: 20px;
-  margin-left: 20px;
 `;
-
-export const CardPurple = styled.View`
-  width: 320px;
-  height: 210px;
-  border-radius: 30px;
-  background-color: #5F59E1;
-  padding: 20px;
-  margin-left: 20px;
-`;
-
-export const Encapsulate = styled.View``;
 
 export const TitleCard = styled.Text`
   font-size: 13px;
@@ -76,5 +70,4 @@ export const RowView = styled.View`
   flex-direction: row;
   width: 100%;
   justify-content: space-evenly;
-  padding: 20px;
 `;
